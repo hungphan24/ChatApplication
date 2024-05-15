d4664f2fb76f8d3e693eaf1077da5ab2371abdba
hungphan <duchungeden@gmail.com>
2024-05-15 22:33:39 +0700

    Fix: error connect to other app

--- main.c
+++ main.c
@@ -3,13 +3,15 @@
 #include <pthread.h>
 #include <string.h>
 #include <arpa/inet.h>
-#include<signal.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-#define     MAX_LENGTH_COMMAND  20
-#define     MAX_LENGTH_MESSAGE  20
+#define MAX_LENGTH_COMMAND 100
+#define MAX_LENGTH_MESSAGE 100
 
 void parseInput(int argc, char const* argv[], int *port) {
-    if(argc < 2) {
+    if (argc < 2) {
         printf("Port number is empty. Please add port number\n");
         exit(1);
     } else {
@@ -21,90 +23,95 @@ void sigintHandler(int sig) {
     terminateSocket(-1);
 }
 
-int main(int argc , char const* argv[]) {
+int main(int argc, char const* argv[]) {
     int shouldExit = 1;
     signal(SIGINT, sigintHandler);
     pthread_t thread_id1;
     char command[MAX_LENGTH_COMMAND];
     int port;
-     parseInput(argc, argv, &port);
+    parseInput(argc, argv, &port);
     displayMenu();
 
     pthread_create(&thread_id1, NULL, socketHandler, (void*) &port);
-    while(shouldExit){
-        fgets(command, sizeof(command), stdin);
+    while (shouldExit) {
+        printf("Enter command: ");
+        if (fgets(command, sizeof(command), stdin) == NULL) {
+            continue;
+        }
         command[strcspn(command, "\n")] = '\0';
 
         // Tokenize the command
         char *token = strtok(command, " ");
         if (token == NULL) {
-            printf("Invalid command0\n");
+            printf("Invalid command\n");
             continue;
         }
 
-        // Compare the command and call appropriate function
+        printf("Command: %s\n", token); // Debugging output
+
+        // Compare the command and call the appropriate function
         if (strcmp(token, "myip") == 0) {
-            displayMyIP();   
+            displayMyIP();
         } else if (strcmp(token, "myport") == 0) {
             displayMyPort();
         } else if (strcmp(token, "list") == 0) {
             listConnection();
         } else if (strcmp(token, "terminate") == 0) {
-            int connectionId;
             token = strtok(NULL, " ");
             if (token == NULL) {
-                printf("Invalid command format2\n");
+                printf("Invalid command format (missing connection ID)\n");
                 continue;
             }
-            connectionId = atoi(token);
+            int connectionId = atoi(token);
             terminateSocket(connectionId);
-            
-        } else if (strncmp(token, "connect", 7) == 0) {
-            char ip[16];
-            int port1;
 
+        } else if (strcmp(token, "connect") == 0) {
             token = strtok(NULL, " ");
             if (token == NULL) {
-                printf("Invalid command format1\n");
+                printf("Invalid command format (missing IP address)\n");
                 continue;
             }
+            char ip[16];
             strncpy(ip, token, sizeof(ip));
             ip[sizeof(ip) - 1] = '\0';
 
             token = strtok(NULL, " ");
             if (token == NULL) {
-                printf("Invalid command format2\n");
+                printf("Invalid command format (missing port number)\n");
                 continue;
             }
-            port1 = atoi(token);
+            char *endptr;
+            int port1 = strtol(token, &endptr, 10);
+            if (*endptr != '\0') {
+                printf("Invalid port number\n");
+                continue;
+            }
             printf("IP: %s, Port: %d\n", ip, port1);
 
             connectToNewSocket(ip, port1);
-        } else if (strncmp(token, "send", 7) == 0) {
-            int connectionId;
+        } else if (strcmp(token, "send") == 0) {
             token = strtok(NULL, " ");
+            if (token == NULL) {
+                printf("Invalid command format (missing connection ID)\n");
+                continue;
+            }
+            int connectionId = atoi(token);
+
+            token = strtok(NULL, " ");
+            if (token == NULL) {
+                printf("Invalid command format (missing message)\n");
+                continue;
+            }
             char message[MAX_LENGTH_MESSAGE];
-            if (token == NULL) {
-                printf("Invalid command format2\n");
-                continue;
-            }
-            connectionId = atoi(token);
-            token = strtok(NULL, " ");
-            if (token == NULL) {
-                printf("Invalid command format1\n");
-                continue;
-            }
             strncpy(message, token, sizeof(message));
             message[sizeof(message) - 1] = '\0';
-            // printf("%d %s\n", connectionId, message);
             sendMessage(connectionId, message);
         } else if (strcmp(token, "exit") == 0) {
             shouldExit = 0;
         } else {
-            printf("Invalid command4\n");
+            printf("Invalid command\n");
         }
     }
-
 
     return 0;
 }
